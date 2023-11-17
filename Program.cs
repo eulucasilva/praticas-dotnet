b388caeef2c07042160ad86b46580dce94e0be80
Lucas Silva <lucas_avlis@outlook.com>
2023-11-16 22:51:36 -0300

    Finalização do exercicio e adicção do README

--- Program.cs
+++ Program.cs
@@ -1,4 +1,65 @@
-﻿#region Exercicio 3
+﻿#region Exercicio 1
+
+/*dotnet --version exibirá a versão do .NET SDK instalada no seu sistema
+
+dotnet --list-sdks lista todas as versões do .NET SDK instaladas no seu sistema
+
+dotnet --uninstall-sdk <versão> remove uma versão específica do .NET SDK
+
+dotnet --version O .NET CLI (Command Line Interface) tem a capacidade de atualizar automaticamente 
+para a versão mais recente do SDK. Portanto, você pode usar o comando dotnet --version para verificar 
+a versão atual e, se necessário, o CLI solicitará a atualização para a versão mais recente. */
+
+#endregion
+
+#region Exercicio 2
+
+/*
+sbyte:
+Tamanho: 8 bits
+Faixa: -128 a 127
+Exemplo: sbyte valor = 10;
+
+byte:
+Tamanho: 8 bits
+Faixa: 0 a 255
+byte valor = 255;
+
+
+short:
+Tamanho: 16 bits
+Faixa: -32,768 a 32,767
+short valor = -15000;
+
+ushort:
+Tamanho: 16 bits
+Faixa: 0 a 65,535
+ushort valor = 50000;
+
+int:
+Tamanho: 32 bits
+Faixa: -2,147,483,648 a 2,147,483,647
+int valor = 1000000;
+
+uint:
+Tamanho: 32 bits
+Faixa: 0 a 4,294,967,295
+uint valor = 3000000000;
+
+
+long:
+Tamanho: 64 bits
+Faixa: -9,223,372,036,854,775,808 a 9,223,372,036,854,775,807
+long valor = 9000000000000000000;
+
+ulong:
+Tamanho: 64 bits
+Faixa: 0 a 18,446,744,073,709,551,615
+ulong valor = 15000000000000000000; */
+
+#endregion
+
+#region Exercicio 3
     double numeroDouble = 10.75;
     int numeroInteiro = (int)numeroDouble;
 
