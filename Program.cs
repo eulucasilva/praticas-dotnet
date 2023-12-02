765496c0bf6f3e0e15a7139ba63084fd5b22fc76
Lucas Silva <lucas_avlis@outlook.com>
2023-12-02 17:16:56 -0300

    Refatorando código

--- Program.cs
+++ Program.cs
@@ -1,122 +1,312 @@
-﻿
+﻿using System.Text.RegularExpressions;
 class Pessoa
 {
-    public string Nome { get; set; }
-    public DateTime DataNascimento { get; set; }
-    public string CPF { get; set; }
+
+    private string? _nome;
+    private DateTime _nascimento;
+    private string? _cpf;
+    public string? Nome
+    {
+        get => _nome;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value)) throw new Exception("Nome não pode ser nulo");
+            _nome = value;
+        }
+    }
+
+    public DateTime Nascimento
+    {
+        get => _nascimento;
+        set
+        {
+            if (value > DateTime.Now) throw new Exception("Data de nascimento inválida");
+            _nascimento = value;
+        }
+    }
+
+    public string? Cpf
+    {
+        get => _cpf;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value) || value.Length != 11) throw new Exception("Cpf não pode ser nulo");
+            _cpf = value;
+        }
+    }
+
+    public virtual void ExibirDetalhes()
+    {
+        Console.WriteLine($"Nome: {Nome}, Idade: {CalcularIdade()} anos");
+    }
 
     public int CalcularIdade()
     {
         DateTime hoje = DateTime.Now;
-        int idade = hoje.Year - DataNascimento.Year - ((hoje.Month > DataNascimento.Month || (hoje.Month == DataNascimento.Month && hoje.Day >= DataNascimento.Day)) ? 0 : 1);
+        int idade = hoje.Year - Nascimento.Year - ((hoje.Month > Nascimento.Month || (hoje.Month == Nascimento.Month && hoje.Day >= Nascimento.Day)) ? 0 : 1);
         return idade;
     }
 }
 
 class Treinador : Pessoa
 {
-    public string CREF { get; set; }
+     private string? _cref;
+
+    // Propriedades
+    public string? Cref
+    {
+        get => _cref;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value)) throw new Exception("Cref não pode ser nulo");
+            _cref = value;
+        }
+    }
+
+    public override void ExibirDetalhes()
+    {
+        base.ExibirDetalhes();
+        Console.WriteLine($"CREF: {Cref}");
+    }
 }
 
 class Cliente : Pessoa
 {
-    public double Altura { get; set; }
-    public double Peso { get; set; }
+    private float _altura;
+    private float _peso;
+    
+    // Propriedades
+    public float Altura
+    {
+        get => _altura;
+        set
+        {
+            if (value < 0) throw new Exception("Altura não pode ser negativa");
+            _altura = value;
+        }
+    }
+    public float Peso
+    {
+        get => _peso;
+        set
+        {
+            if (value < 0) throw new Exception("Peso não pode ser negativo");
+            _peso = value;
+        }
+    }
 
     public double CalcularIMC()
     {
         return Peso / (Altura * Altura);
     }
+
+    public static bool validaData(string data)
+    {
+        const string padrao = @"\d{2}/\d{2}/\d{4}";
+        if (Regex.IsMatch(data, padrao)) return true;
+        return false;
+    }
+
+    public static bool validaCpf(string cpf)
+    {
+        if (Regex.IsMatch(cpf, @"^\d+$") && cpf.Length == 11)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static bool validaCref(string cref)
+    {
+        const string padrao = @"\d{6}-\d{1}/[A-Z]{2}";
+        if (Regex.IsMatch(cref, padrao))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static int calculaIdade(DateTime dataNascimento)
+    {
+        DateTime hoje = DateTime.Today;
+        int idade = hoje.Year - dataNascimento.Year;
+        if (dataNascimento > hoje.AddYears(-idade))
+            idade--;
+        return idade;
+    }
 }
 
 class Program
 {
+    static List<Pessoa> pessoas = new List<Pessoa>();
     static void Main()
     {
-        List<Treinador> treinadores = new List<Treinador>();
-        List<Cliente> clientes = new List<Cliente>();
 
-        try
-        {
-            Treinador treinador1 = new Treinador { Nome = "Treinador1", DataNascimento = new DateTime(1980, 5, 15), CPF = "12345678901", CREF = "CREF123" };
-            Treinador treinador2 = new Treinador { Nome = "Treinador2", DataNascimento = new DateTime(1990, 10, 20), CPF = "98765432101", CREF = "CREF456" };
-            treinadores.Add(treinador1);
-            treinadores.Add(treinador2);
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
-        }
+        AdicionarPessoa(new Treinador { Nome = "João Treinador", Nascimento = new DateTime(1980, 5, 15), Cpf = "12345678901", Cref = "CREF123" });
+        AdicionarPessoa(new Treinador { Nome = "Maria Treinadora", Nascimento = new DateTime(1990, 10, 20), Cpf = "98765432101", Cref = "CREF456" });
+
+        AdicionarPessoa(new Cliente { Nome = "Carlos Cliente", Nascimento = new DateTime(1995, 3, 10), Cpf = "11122233344", Altura = 1.75F, Peso = 75 });
+        AdicionarPessoa(new Cliente { Nome = "Ana Cliente", Nascimento = new DateTime(1988, 12, 5), Cpf = "55566677788", Altura = 1.60F, Peso = 65 });
+
+        Console.WriteLine("Lista de Pessoas:");
+        ListarPessoas();
+
+        Console.WriteLine("\nAtualizar Pessoa:");
+        AtualizarPessoa("11122233344", new Cliente { Nome = "Carlos Cliente Atualizado", Nascimento = new DateTime(1995, 3, 10), Cpf = "11122233344", Altura = 1.80F, Peso = 80 });
+        ListarPessoas();
+
+        Console.WriteLine("\nRemover Pessoa:");
+        RemoverPessoa("CREF123");
+        ListarPessoas();
+
+        Console.WriteLine("\nRelatório: Treinadores com idade entre 25 e 40 anos");
+        RelatorioTreinadoresIdade(25, 40);
+
+        Console.WriteLine("\nRelatório: Clientes com idade entre 20 e 35 anos");
+        RelatorioClientesIdade(20, 35);
+
+        Console.WriteLine("\nRelatório: Clientes com IMC maior que 25 em ordem crescente");
+        RelatorioClientesIMC(25);
+
+        Console.WriteLine("\nRelatório: Clientes em ordem alfabética");
+        RelatorioClientesOrdemAlfabetica();
+
+        Console.WriteLine("\nRelatório: Clientes do mais velho para o mais novo");
+        RelatorioClientesOrdemIdade();
+
+        Console.WriteLine("\nRelatório: Aniversariante do mês");
+        RelatorioAniversariantesDoMes();
 
 
-        try
-        {
-            Cliente cliente1 = new Cliente { Nome = "Cliente1", DataNascimento = new DateTime(1995, 3, 10), CPF = "11122233344", Altura = 1.75, Peso = 75 };
-            Cliente cliente2 = new Cliente { Nome = "Cliente2", DataNascimento = new DateTime(1988, 12, 5), CPF = "55566677788", Altura = 1.60, Peso = 65 };
-            clientes.Add(cliente1);
-            clientes.Add(cliente2);
-
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
-        }
-
-
-
-        Console.WriteLine("Treinadores com idade entre 25 e 40 anos:");
-        foreach (var treinador in RelatorioTreinadoresIdade(treinadores, 25, 40))
-        {
-            Console.WriteLine($"{treinador.Nome}, {treinador.CalcularIdade()} anos");
-        }
-
-        Console.WriteLine("\nClientes com idade entre 20 e 35 anos:");
-        foreach (var cliente in RelatorioClientesIdade(clientes, 20, 35))
-        {
-            Console.WriteLine($"{cliente.Nome}, {cliente.CalcularIdade()} anos");
-        }
-
-        Console.WriteLine("\nClientes com IMC maior que 25 em ordem crescente:");
-        foreach (var cliente in RelatorioClientesIMC(clientes, 25))
-        {
-            Console.WriteLine($"{cliente.Nome}, IMC: {cliente.CalcularIMC()}");
-        }
-
-        Console.WriteLine("\nClientes em ordem alfabética:");
-        foreach (var cliente in RelatorioClientesOrdemAlfabetica(clientes))
-        {
-            Console.WriteLine($"{cliente.Nome}");
-        }
-
-        Console.WriteLine("\nClientes do mais velho para o mais novo:");
-        foreach (var cliente in RelatorioClientesOrdemIdade(clientes))
-        {
-            Console.WriteLine($"{cliente.Nome}, {cliente.CalcularIdade()} anos");
-        }
     }
 
-    static List<Treinador> RelatorioTreinadoresIdade(List<Treinador> treinadores, int minIdade, int maxIdade)
+    static DateTime ValidarData(string dataStr)
     {
-        return treinadores.Where(t => t.CalcularIdade() >= minIdade && t.CalcularIdade() <= maxIdade).ToList();
+        DateTime data;
+        while (!DateTime.TryParse(dataStr, out data) || data > DateTime.Now)
+        {
+            Console.Write("Data inválida ou no futuro. Digite novamente (YYYY-MM-DD): ");
+            dataStr = Console.ReadLine();
+        }
+        return data;
     }
 
-    static List<Cliente> RelatorioClientesIdade(List<Cliente> clientes, int minIdade, int maxIdade)
+    static string ValidarCPF(string cpf)
     {
-        return clientes.Where(c => c.CalcularIdade() >= minIdade && c.CalcularIdade() <= maxIdade).ToList();
+        while (!IsValidCPF(cpf))
+        {
+            Console.Write("CPF inválido. Digite novamente (XXX.XXX.XXX-XX): ");
+            cpf = Console.ReadLine();
+        }
+        return cpf;
     }
 
-    static List<Cliente> RelatorioClientesIMC(List<Cliente> clientes, double minIMC)
+    static bool IsValidCPF(string cpf)
     {
-        return clientes.Where(c => c.CalcularIMC() > minIMC).OrderBy(c => c.CalcularIMC()).ToList();
+        return cpf.Length == 14 && cpf.Count(char.IsDigit) == 11;
     }
 
-    static List<Cliente> RelatorioClientesOrdemAlfabetica(List<Cliente> clientes)
+    static void RelatorioTreinadoresIdade(int minIdade, int maxIdade)
     {
-        return clientes.OrderBy(c => c.Nome).ToList();
+        var treinadores = pessoas.OfType<Treinador>().Where(t => t.CalcularIdade() >= minIdade && t.CalcularIdade() <= maxIdade).ToList();
+        foreach (var treinador in treinadores)
+        {
+            treinador.ExibirDetalhes();
+            Console.WriteLine();
+        }
     }
 
-    static List<Cliente> RelatorioClientesOrdemIdade(List<Cliente> clientes)
+    static void RelatorioClientesIdade(int minIdade, int maxIdade)
     {
-        return clientes.OrderByDescending(c => c.CalcularIdade()).ToList();
+        var clientes = pessoas.OfType<Cliente>().Where(c => c.CalcularIdade() >= minIdade && c.CalcularIdade() <= maxIdade).ToList();
+        foreach (var cliente in clientes)
+        {
+            cliente.ExibirDetalhes();
+            Console.WriteLine();
+        }
     }
+
+    static void RelatorioClientesIMC(double minIMC)
+    {
+        var clientes = pessoas.OfType<Cliente>().Where(c => c.CalcularIMC() > minIMC).OrderBy(c => c.CalcularIMC()).ToList();
+        foreach (var cliente in clientes)
+        {
+            cliente.ExibirDetalhes();
+            Console.WriteLine();
+        }
+    }
+
+    static void RelatorioClientesOrdemAlfabetica()
+    {
+        var clientes = pessoas.OfType<Cliente>().OrderBy(c => c.Nome).ToList();
+        foreach (var cliente in clientes)
+        {
+            cliente.ExibirDetalhes();
+            Console.WriteLine();
+        }
+    }
+
+    static void RelatorioClientesOrdemIdade()
+    {
+        var clientes = pessoas.OfType<Cliente>().OrderByDescending(c => c.CalcularIdade()).ToList();
+        foreach (var cliente in clientes)
+        {
+            cliente.ExibirDetalhes();
+            Console.WriteLine();
+        }
+    }
+
+    static void RelatorioAniversariantesDoMes()
+    {
+        int mesAtual = DateTime.Now.Month;
+        var aniversariantes = pessoas.Where(p => p.Nascimento.Month == mesAtual).ToList();
+
+        foreach (var pessoa in aniversariantes)
+        {
+            pessoa.ExibirDetalhes();
+            Console.WriteLine();
+        }
+    }
+    static void AdicionarPessoa(Pessoa pessoa)
+    {
+        pessoas.Add(pessoa);
+    }
+
+    static void ListarPessoas()
+    {
+        foreach (var pessoa in pessoas)
+        {
+            pessoa.ExibirDetalhes();
+            Console.WriteLine();
+        }
+    }
+
+    static void AtualizarPessoa(string cpf, Pessoa pessoaAtualizada)
+    {
+        Pessoa pessoaExistente = pessoas.FirstOrDefault(p => p.Cpf == cpf);
+        if (pessoaExistente != null)
+        {
+            pessoaExistente.Nome = pessoaAtualizada.Nome;
+            pessoaExistente.Nascimento = pessoaAtualizada.Nascimento;
+
+            if (pessoaExistente is Cliente clienteExistente && pessoaAtualizada is Cliente clienteAtualizado)
+            {
+                clienteExistente.Altura = clienteAtualizado.Altura;
+                clienteExistente.Peso = clienteAtualizado.Peso;
+            }
+        }
+    }
+
+    static void RemoverPessoa(string cref)
+    {
+        Pessoa pessoaExistente = pessoas.FirstOrDefault(p => p is Treinador && ((Treinador)p).Cref == cref);
+        if (pessoaExistente != null)
+        {
+            pessoas.Remove(pessoaExistente);
+        }
+    }
+
+
 }
